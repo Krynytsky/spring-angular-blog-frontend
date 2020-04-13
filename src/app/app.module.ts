1b1c3371e8b6226b7969039668960cfe64a884df
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-04-13 13:39:34 +0300

    create register form; add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {RegisterSucessComponent} from './auth/register-sucess/register-sucess.component';
+import {LoginComponent} from './auth/login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
+// const appRoutes: Routes = [
+//   { path: 'register', component: RegisterComponent }
+// ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+    RegisterSucessComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      [
+        { path: 'register', component: RegisterComponent }
+      ]
+      // appRoutes,
+      // { enableTracing: true }
+      ),
+    //
+    //   {path: 'register', component: RegisterComponent},
+    // ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
